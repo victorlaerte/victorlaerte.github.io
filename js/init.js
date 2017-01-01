0ee983e99fe060221a1dd9b80ca4f0dc1341505f
victorlaerte <victorlaertedoliveira@gmail.com>
2017-01-01 20:30:21 -0300

    indent and deleting unused files

--- js/init.js
+++ js/init.js
@@ -4,99 +4,68 @@
 /*
 -----------------------------------------------------------------------------------*/
 
- jQuery(document).ready(function($) {
+jQuery(document).ready(function($) {
 
-/*----------------------------------------------------*/
-/* FitText Settings
------------------------------------------------------- */
+    /*----------------------------------------------------*/
+    /* FitText Settings
+    ------------------------------------------------------ */
 
     setTimeout(function() {
-	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
-	 }, 100);
+        $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
+    }, 100);
 
 
-/*----------------------------------------------------*/
-/* Smooth Scrolling IMPLEMENT IN ANGULAR
------------------------------------------------------- */
+    /*----------------------------------------------------*/
+    /* Smooth Scrolling IMPLEMENT IN ANGULAR
+    ------------------------------------------------------ */
 
-   $('.smoothscroll').on('click',function (e) {
-	    e.preventDefault();
+    $('.smoothscroll').on('click', function(e) {
+        e.preventDefault();
 
-	    var target = this.hash,
-	    $target = $(target);
+        var target = this.hash,
+            $target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 800, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
 
 
-/*----------------------------------------------------*/
-/*	Make sure that #header-background-image height is
-/* equal to the browser height.
------------------------------------------------------- */
+    /*----------------------------------------------------*/
+    /*	Make sure that #header-background-image height is
+    /* equal to the browser height.
+    ------------------------------------------------------ */
 
-   $('header').css({ 'height': $(window).height() });
-   $(window).on('resize', function() {
+    $('header').css({ 'height': $(window).height() });
+    $(window).on('resize', function() {
 
         $('header').css({ 'height': $(window).height() });
         $('body').css({ 'width': $(window).width() })
-   });
-
-
-/*----------------------------------------------------*/
-/*	Fade In/Out Primary Navigation
-------------------------------------------------------*/
-
-   $(window).on('scroll', function() {
-
-		var h = $('header').height();
-		var y = $(window).scrollTop();
-      var nav = $('#nav-wrap');
-
-	   if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
-	      nav.fadeOut('fast');
-	   }
-      else {
-         if (y < h*.20) {
-            nav.removeClass('opaque').fadeIn('fast');
-         }
-         else {
-            nav.addClass('opaque').fadeIn('fast');
-         }
-      }
-
-	});
-
-/*----------------------------------------------------*/
-/*	Modal Popup
------------------------------------------------------- 
-
-
-    $('.item-wrap a').magnificPopup({
-
-       type:'inline',
-       fixedContentPos: false,
-       removalDelay: 200,
-       showCloseBtn: false,
-       mainClass: 'mfp-fade'
-
     });
 
-    $(document).on('click', '.popup-modal-dismiss', function (e) {
-    		e.preventDefault();
-    		$.magnificPopup.close();
+
+    /*----------------------------------------------------*/
+    /*	Fade In/Out Primary Navigation
+    ------------------------------------------------------*/
+
+    $(window).on('scroll', function() {
+
+        var h = $('header').height();
+        var y = $(window).scrollTop();
+        var nav = $('#nav-wrap');
+
+        if ((y > h * .20) && (y < h) && ($(window).outerWidth() > 768)) {
+            nav.fadeOut('fast');
+        } else {
+            if (y < h * .20) {
+                nav.removeClass('opaque').fadeIn('fast');
+            } else {
+                nav.addClass('opaque').fadeIn('fast');
+            }
+        }
+
     });
-*/
 
 });
-
-
-
-
-
-
-
-
